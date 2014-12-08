2737d9193de0ac850a13b578a10fbc733d56dfc4
manos <rmanolis@live.com>
2014-12-08 20:15:20 +0200

    Adding facebook

--- public/app/controllers/HomeCtrl.js
+++ public/app/controllers/HomeCtrl.js
@@ -1,3 +1,13 @@
-app.controller('HomeCtrl', function($scope){
+app.controller('HomeCtrl', function($scope, Facebook){
+  
+          $scope.login = function () {
+            Facebook.login(function(response) {
+              if (response.status == 'connected') {
+                $scope.status = 'yes';
+              } else {
+                $scope.status = 'no';
+              }
+            });
+          };
 
 });
