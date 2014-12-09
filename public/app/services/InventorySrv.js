7c1835728a391e8809f34928281fd4df2c4cf0d8
manos <rmanolis@live.com>
2014-12-09 20:35:08 +0200

    Fixing https

--- public/app/services/InventorySrv.js
+++ public/app/services/InventorySrv.js
@@ -2,25 +2,25 @@ app.factory('InventorySrv', function($http){
   var obj = {};
 
   obj.getInventories = function(){
-    return $http.get('https://178.62.126.138/inventories');
+    return $http.get('https://178.62.126.138/inventories?callback=JSON_CALLBACK');
   }
 
   obj.postInventory = function(name){
-    return $http.post('https://178.62.126.138/inventories/', {'name':name});
+    return $http.post('https://178.62.126.138/inventories/?callback=JSON_CALLBACK', {'name':name});
   }
   
   obj.deleteInventory = function(inventory_id){
-    return $http.delete('https://178.62.126.138/inventories/'+inventory_id);
+    return $http.delete('https://178.62.126.138/inventories/'+inventory_id + '?callback=JSON_CALLBACK');
   }
 
   obj.getScrollsFromInventory = function(inventory_id){
-    return $http.get('https://178.62.126.138/inventories/'+inventory_id +'/scrolls');
+    return $http.get('https://178.62.126.138/inventories/'+inventory_id +'/scrolls?callback=JSON_CALLBACK');
   }
 
   obj.putScrollToAnotherInventory= function(
       from_inv_id,to_inv_id, scroll_id){
     return $http.put('https://178.62.126.138/inventories/'+from_inv_id + 
-        '/scrolls/'+scroll_id + '/inventories/' + to_inv_id);
+        '/scrolls/'+scroll_id + '/inventories/' + to_inv_id + '?callback=JSON_CALLBACK');
   }
 
       
--- public/app/services/ScrollSrv.js
+++ public/app/services/ScrollSrv.js
@@ -2,15 +2,15 @@ app.factory('ScrollSrv', function($http){
   var obj={};
  
   obj.postScroll = function(scroll){
-    return $http.post('https://178.62.126.138/scrolls/',scroll);
+    return $http.post('https://178.62.126.138/scrolls/?callback=JSON_CALLBACK',scroll);
   }
 
   obj.getScroll = function(scroll_id){
-    return $http.get('https://178.62.126.138/scrolls/'+scroll_id);
+    return $http.get('https://178.62.126.138/scrolls/'+scroll_id + '?callback=JSON_CALLBACK');
   }
 
   obj.getUnfinishedScrolls = function(){
-    return $http.get('https://178.62.126.138/scrolls/unfinished');
+    return $http.get('https://178.62.126.138/scrolls/unfinished?callback=JSON_CALLBACK');
   }
 
   return obj;
