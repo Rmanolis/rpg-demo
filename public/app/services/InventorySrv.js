6aff16e87bdb0a514646164cd302691a5d359af5
manos <rmanolis@live.com>
2014-12-09 20:39:51 +0200

    Fixing https

--- public/app/services/InventorySrv.js
+++ public/app/services/InventorySrv.js
@@ -2,25 +2,25 @@ app.factory('InventorySrv', function($http){
   var obj = {};
 
   obj.getInventories = function(){
-    return $http.get('https://178.62.126.138/inventories?callback=JSON_CALLBACK');
+    return $http.jsonp('https://178.62.126.138/inventories?callback=JSON_CALLBACK');
   }
 
   obj.postInventory = function(name){
-    return $http.post('https://178.62.126.138/inventories/?callback=JSON_CALLBACK', {'name':name});
+    return $http.post('https://178.62.126.138/inventories/', {'name':name});
   }
   
   obj.deleteInventory = function(inventory_id){
-    return $http.delete('https://178.62.126.138/inventories/'+inventory_id + '?callback=JSON_CALLBACK');
+    return $http.delete('https://178.62.126.138/inventories/'+inventory_id);
   }
 
   obj.getScrollsFromInventory = function(inventory_id){
-    return $http.get('https://178.62.126.138/inventories/'+inventory_id +'/scrolls?callback=JSON_CALLBACK');
+    return $http.jsonp('https://178.62.126.138/inventories/'+inventory_id +'/scrolls?callback=JSON_CALLBACK');
   }
 
   obj.putScrollToAnotherInventory= function(
       from_inv_id,to_inv_id, scroll_id){
     return $http.put('https://178.62.126.138/inventories/'+from_inv_id + 
-        '/scrolls/'+scroll_id + '/inventories/' + to_inv_id + '?callback=JSON_CALLBACK');
+        '/scrolls/'+scroll_id + '/inventories/' + to_inv_id);
   }
 
       
