cb600fe604a7b7c6a10e66e3489cf712eddd81f0
manos <rmanolis@live.com>
2014-12-09 20:22:09 +0200

    Fixing https

--- public/app/services/InventorySrv.js
+++ public/app/services/InventorySrv.js
@@ -2,24 +2,24 @@ app.factory('InventorySrv', function($http){
   var obj = {};
 
   obj.getInventories = function(){
-    return $http.get('/inventories');
+    return $http.get('https://178.62.126.138/inventories');
   }
 
   obj.postInventory = function(name){
-    return $http.post('/inventories/', {'name':name});
+    return $http.post('https://178.62.126.138/inventories/', {'name':name});
   }
   
   obj.deleteInventory = function(inventory_id){
-    return $http.delete('/inventories/'+inventory_id);
+    return $http.delete('https://178.62.126.138/inventories/'+inventory_id);
   }
 
   obj.getScrollsFromInventory = function(inventory_id){
-    return $http.get('/inventories/'+inventory_id +'/scrolls');
+    return $http.get('https://178.62.126.138/inventories/'+inventory_id +'/scrolls');
   }
 
   obj.putScrollToAnotherInventory= function(
       from_inv_id,to_inv_id, scroll_id){
-    return $http.put('/inventories/'+from_inv_id + 
+    return $http.put('https://178.62.126.138/inventories/'+from_inv_id + 
         '/scrolls/'+scroll_id + '/inventories/' + to_inv_id);
   }
 
